6cb25d7b6422abbc63562fb290e08e3b203eea9e
Abdalrahman-Zatary <abd.alrahman.zitre.2005@gmail.com>
2025-12-27 16:56:20 +0300

    feat: add JS interactivity and animation scroll

---
+++ assets/js/main.js
@@ -0,0 +1,41 @@
+// spinner
+const loadingTime = 3000;
+
+window.addEventListener("load", () => {
+  const loader = document.querySelector(".loader");
+  setTimeout(() => {
+    loader.classList.add("hidden");
+  }, loadingTime);
+});
+
+// Timer
+
+const targetDate = new Date("Nov 16, 2026 00:00:00").getTime();
+
+const countdown = setInterval(() => {
+  const now = new Date().getTime();
+  const distance = targetDate - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("days").textContent = String(days).padStart(2, "0");
+  document.getElementById("hours").textContent = String(hours).padStart(2, "0");
+  document.getElementById("minutes").textContent = String(minutes).padStart(
+    2,
+    "0"
+  );
+  document.getElementById("seconds").textContent = String(seconds).padStart(
+    2,
+    "0"
+  );
+
+  if (distance < 0) {
+    clearInterval(countdown);
+    document.querySelector(".countdown").innerHTML = "EXPIRED";
+  }
+}, 1000);
